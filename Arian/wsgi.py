716a3da70e0504b9ef76237c328d4fc00373e062
arigavincent <149542181+arigavincent@users.noreply.github.com>
2025-11-19 13:09:03 +0300

    Update WSGI config to include project paths
    Added project directories to the Python path for WSGI.

--- Arian/wsgi.py
+++ Arian/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add project directories to the Python path
+sys.path.append('/home/Anita/ArianHospital1')   # outer folder
+sys.path.append('/home/Anita/ArianHospital1/Arian')  # inner folder containing settings.py
 
+# Set the settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Arian.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

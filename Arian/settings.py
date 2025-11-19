1f9ebd94f78e521af38b4bb1a19b01fca1ffc8fd
arigavincent <149542181+arigavincent@users.noreply.github.com>
2025-11-19 11:52:00 +0300

    Change database settings to use dj_database_url
    Updated database configuration to use dj_database_url for dynamic database URL management.

--- Arian/settings.py
+++ Arian/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=wte)=)cky%v9yn78%khmfir410(&9eulxzvax1gk!_8-4m_gb'
+#SECRET_KEY = 'django-insecure-=wte)=)cky%v9yn78%khmfir410(&9eulxzvax1gk!_8-4m_gb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'Arian.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
@@ -131,5 +128,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 

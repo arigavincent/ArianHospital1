1c6161c9f3bf4043251a1d3138308cc3da35c73d
arigavincent <149542181+arigavincent@users.noreply.github.com>
2025-11-19 13:21:53 +0300

    Update wsgi.py

--- Arian/wsgi.py
+++ Arian/wsgi.py
@@ -11,8 +11,13 @@ import os
 import sys
 
 # Add project directories to the Python path
-sys.path.append('/home/Anita/ArianHospital1')   # outer folder
-sys.path.append('/home/Anita/ArianHospital1/Arian')  # inner folder containing settings.py
+sys.path.append('/home/Anita/ArianHospital1')       # outer folder
+sys.path.append('/home/Anita/ArianHospital1/Arian') # inner folder containing settings.py
+
+# Activate your virtualenv
+activate_this = '/home/Anita/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 # Set the settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Arian.settings')

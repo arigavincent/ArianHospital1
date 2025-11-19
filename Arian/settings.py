3c5d123e12ca54e208ff2c90d90018b05130cca4
arigavincent <149542181+arigavincent@users.noreply.github.com>
2025-11-19 12:27:04 +0300

    Update settings.py

--- Arian/settings.py
+++ Arian/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-=wte)=)cky%v9yn78%khmfir410(&9eulxzvax1gk!_8-4m_gb'
+SECRET_KEY = 'django-insecure-=wte)=)cky%v9yn78%khmfir410(&9eulxzvax1gk!_8-4m_gb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,6 +133,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
